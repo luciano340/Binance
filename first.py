8761e0ef5fed7160a8729a6538c1cb3282fa8227
Luciano <lucianosouzaromao@gmail.com>
2022-03-26 10:26:31 -0300

    acompanhar

--- first.py
+++ first.py
@@ -1,6 +1,8 @@
 from binance.client import Client
 from secrets import api_key, api_secret
+from web_bot import bot_work
 import logging
+import re
 
 logging.basicConfig(filename='general.log', encoding='utf-8', level=logging.DEBUG,
 format='%(asctime)s - %(levelname)s - %(pathname)s on Line: %(lineno)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
@@ -21,7 +23,15 @@ assets = client.futures_exchange_info()
 
 asset_list = list()
 for k in assets['symbols']:
+    if re.search(r'[0-9]$', k['symbol']):
+        continue
+
     asset_list.append(k['symbol'])
 
 logging.debug(f'Total assets to be used: {len(asset_list)}')
 logging.info(f'Assents found {asset_list}')
+
+i = asset_list[-1]
+
+bot = bot_work(i)
+bot.start_stream()
--- web_bot.py
+++ web_bot.py
@@ -1,24 +1,77 @@
-import websocket
-import logging
+import websocket, json, talib, numpy, logging
+
 
 class bot_work:
     def __init__(self, coin):
-        seelf.coin = coin
+        self.coin = coin.lower()
+        self.closed_values = list()
+        self.high_values = list()
+        self.low_values = list()
+        self.volume_values = list()
+
+    def reset_lists(self):
+        logging.debug(f'Reset informations lists for {self.coin}')
+        self.closed_values = list()
+        self.high_values = list()
+        self.low_values = list()
+        self.volume_values = list()
 
     def on_open(self, ws):
-        logging.info(f'Opened connection on stream for coin {self.coin}')
+        logging.debug(f'Opened connection on stream for coin {self.coin}')
         print(f'Opened connection on stream for coin {self.coin}')
 
     def on_close(self, ws):
-        logging.info(f'Closed connection on stream for coin {self.coin}')
+        logging.debug(f'Closed connection on stream for coin {self.coin}')
         print(f'Closed connection on stream for coin {self.coin}')
 
     def on_messege(self, ws, messege):
-        logging.info(f'Received messege from websocket {self.coin}')
-        print(f'Received messege from websocket {self.coin}')
+        RSI_PERIOD = 14
+        RSI_OVERBOUGHT = 80
+        RSI_OVERSOLD = 20
+        MFI_OVERBOUGHT = 90
+        MFI_OVERSOLD = 10
+
+        logging.debug(f'Received messege from websocket {self.coin}')
+        infos = json.loads(messege)
+        candle = infos['k']
+        is_candle_closed = candle['x']
+        close = candle['c']
+        high = candle['h']
+        low =  candle['l']
+        volume = candle['q']
+
+        if not is_candle_closed:
+            return None
+
+        self.closed_values.append(float(close))
+        self.high_values.append(float(high))
+        self.low_values.append(float(low))
+        self.volume_values.append(float(volume))
+
+        if len(self.closed_values) <= RSI_PERIOD:
+            return None
+
+        np_closes = numpy.array(self.closed_values)
+        np_high = numpy.array(self.high_values)
+        np_low = numpy.array(self.low_values)
+        np_volume = numpy.array(self.volume_values)
+
+        rsi = talib.RSI(np_closes, RSI_PERIOD)
+        last_rsi = rsi[-1]
+        mfi = talib.MFI(np_high, np_low, np_closes, np_volume, RSI_PERIOD)
+        last_mfi = mfi[-1]
+
+        logging.debug(f'Last for {self.coin}: {last_rsi} - Last MFI for {mfi} {self.coin}: {last_mfi}')
         
+        if last_rsi <= RSI_OVERSOLD and last_mfi <= MFI_OVERSOLD:
+            logging.warn(f'{self.coin} Em tentencia de long')
+        elif last_rsi >= RSI_OVERBOUGHT and last_mfi >= MFI_OVERBOUGHT:
+            logging.warn(f'{self.coin} Em tentencia de sort')
+            
+        self.reset_lists()
+
     def start_stream(self):
         SOCKET = f"wss://stream.binance.com:9443/ws/{self.coin}@kline_1m"
-        RSI_PERIOD = 15
-        ws = websocket(SOCKET, on_open=self.on_open, on_close=, on_message=)
+        websocket.enableTrace(False)
+        ws = websocket.WebSocketApp(SOCKET, on_open=self.on_open, on_message=self.on_messege)
         ws.run_forever()
